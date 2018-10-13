a487d380bc715cb2365255b9fe4a58478da3da6a
Guido Gerosa <gerosa1990@yahoo.it>
2018-10-13 18:37:14 +0200

    Panel unit test completed

--- tests/unit/panel.js
+++ tests/unit/panel.js
@@ -1,42 +1,67 @@
 describe("Panel", function () {
     beforeAll(function(){
-        this.domService = new DOMService();
-    })
-    it("expects subtitled PanelModel in constructor", function () {
-        var subtitledPanel = {
+        this.domService = new DOMService()
+        this.subtitledPanel = {
             title: 'Title 1',
             subtitle: 'subtitle 1',
             content: '<p>HTML content 1</p>'
         };
+        this.subtitlelessPanel = {
+            title: 'Title 1',
+            content: '<p>HTML content 1</p>'
+        };
+    })
+    it("expects subtitled PanelModel in constructor", function () {
         try{
-            var p = new Panel(subtitledPanel,this.domService);
+            var p = new Panel(this.subtitledPanel,this.domService);
         }
         catch(e){
             fail(e);
         }
     });
     it("expects subtitleless PanelModel in constructor", function () {
-        var subtitlelessPanel = {
-            title: 'Title 1',
-            content: '<p>HTML content 1</p>'
-        };
         try{
-            var p = new Panel(subtitlelessPanel,this.domService);
+            var p = new Panel(this.subtitlelessPanel,this.domService);
         }
         catch(e){
             fail(e);
         }
     });
     it("refuses objects not PanelModel", function () {
+        var refused = {foo:'bar'};
         try{
-            var p = new Panel(subtitlelessPanel,this.domService);
+            var p = new Panel(refused,this.domService);
         }
         catch(e){
             return; //test passed
         }
         fail("No exception raised");
     });
-    it("can be toggleOpen()ed to switch state", function () {
-        expect(true).toBe(true);
+    it('creates correct content',function(){
+        var p = new Panel(this.subtitledPanel,this.domService);
+    });
+    it('toggle with toggle method', function(){
+        var p = new Panel(this.subtitledPanel,this.domService);
+        expect(p.htmlElement.classList).toContain('item-closed');
+        p.toggle();
+        expect(p.htmlElement.classList).not.toContain('item-closed');
+        p.toggle();
+        expect(p.htmlElement.classList).toContain('item-closed');
+    });
+    it('toggle with dropdown button (class item-dropdown)', function(){
+        var p = (new Panel(this.subtitledPanel,this.domService)).htmlElement;
+        expect(p.classList).toContain('item-closed');
+        p.getElementsByClassName('item-dropdown')[0].onclick();
+        expect(p.classList).not.toContain('item-closed');
+        p.getElementsByClassName('item-dropdown')[0].onclick();
+        expect(p.classList).toContain('item-closed');
+    });
+    it('toggle with title (class item-title)', function(){
+        var p = (new Panel(this.subtitledPanel,this.domService)).htmlElement;
+        expect(p.classList).toContain('item-closed');
+        p.getElementsByClassName('item-title')[0].onclick();
+        expect(p.classList).not.toContain('item-closed');
+        p.getElementsByClassName('item-title')[0].onclick();
+        expect(p.classList).toContain('item-closed');
     });
 });
