5b8b603c9ddec5ed34fad3a6f32c2e5c9feae5bb
Guido Gerosa <gerosa1990@yahoo.it>
2018-10-14 16:12:46 +0200

    Documented source

--- ts/g-accordion.ts
+++ ts/g-accordion.ts
@@ -1,20 +1,38 @@
+/**
+ * Represents an accordion panel's data
+ */
 class PanelModel {
     title: string
-    subtitle: string
+    subtitle?: string
     content: string
-    static isValid(o: PanelModel): Boolean {
-        if (!o) return false;
-        if (!o.title) return false;
-        if (!o.content) return false;
+
+    /**
+     * Checks wether the given data is compatible with the PanelModel class.
+     * @param panel The PanelModel to verify
+     * @returns true when the given panel is compatible with PanelModel, otherwise false
+     */
+    static isValid(panel: PanelModel): Boolean {
+        if (!panel) return false;
+        if (!panel.title) return false;
+        if (!panel.content) return false;
         // TODO Check types
         // TODO investigate more transparent ways to do runtime type checking (https://github.com/fabiandev/ts-runtime)
         return true
     }
 }
 
+/**
+ * HTML UI class for PanelModel
+ */
 class GPanel {
+    /**
+     * the source PanelModel used in constructor
+     */
     model: PanelModel
     private _isOpen: Boolean
+    /**
+     * the rendered HTML root element
+     */
     htmlElement: HTMLElement
 
     private setStatus(open: Boolean): void {
@@ -26,12 +44,23 @@ class GPanel {
         }
     }
 
+    /**
+     * Tells wether the GPanel is open or closed
+     * @returns true when open, false when closed
+     */
     isOpen():Boolean {return this._isOpen;}
 
+    /**
+     * Toggles between open and closed state
+     */
     toggle() {
         this.setStatus(!this._isOpen);
     }
 
+    /**
+     * Builds the object using the given PanelModel
+     * @param model PanelModel to use
+     */
     constructor(model: PanelModel) {
         if (!model) {
             throw new Error("PanelModel is null");
@@ -85,16 +114,27 @@ class GPanel {
     }
 
 }
-
+/**
+ * Represents accordion data
+ */
 class AccordionModel {
-    container: string  // id of DOM element to build onto
+    /**
+     * id of DOM element to build onto
+     */
+    container: string
     mainTitle?: string
     panels: PanelModel[]
-    static isValid(o: AccordionModel): Boolean {
-        if (!o) return false;
-        if (!o.container) return false;
-        if (!o.panels) return false;
-        for (let p of o.panels) {
+
+    /**
+     * Checks wether the given data is compatible with the AccordionModel class.
+     * @param accordion The AccordionModel to verify
+     * @returns true when the given panel is compatible with AccordionModel, otherwise false
+     */
+    static isValid(accordion: AccordionModel): Boolean {
+        if (!accordion) return false;
+        if (!accordion.container) return false;
+        if (!accordion.panels) return false;
+        for (let p of accordion.panels) {
             if (!PanelModel.isValid(p)) return false;
         }
         // TODO Check types
@@ -103,24 +143,42 @@ class AccordionModel {
     }
 }
 
+/**
+ * HTML UI class for AccordionModel.
+ */
 class GAccordion {
+    /**
+     * the source AccordionModel used in constructor
+     */
     model: AccordionModel
 
+    /**
+     * the rendered HTML root element
+     */
     htmlElement: HTMLElement
+
+    /**
+     * this instance's GPanels
+     */
     panels: GPanel[]
 
-    constructor(model: AccordionModel, $domService: IDOM) {
+    /**
+     * Builds the object using the given AccordionModel
+     * @param model AccordionModel to use 
+     * @param $dom the IDOM to build the accordion in
+     */
+    constructor(model: AccordionModel, $dom: IDOM) {
         if (!model) {
             throw new Error("AccordionModel is null");
         }
         if (!AccordionModel.isValid(model)) {
             throw new Error("AccordionModel is not valid");
         }
-        if (!$domService) {
+        if (!$dom) {
             throw new Error("IDOMService dependency is null");
         }
         this.model = model;
-        let DOMcontainer = $domService.findById(this.model.container);
+        let DOMcontainer = $dom.findById(this.model.container);
         DOMcontainer.classList.add('g-accordion');
 
         if (this.model.mainTitle) {
@@ -142,12 +200,21 @@ class GAccordion {
     }
 }
 
+/**
+ * Represents a HTML element description
+ */
 class HTMLElementModel {
     tagname: string
     innerHTML?: string
     classList?: string[]
     children?: HTMLElement[]
     onclick?: any
+    
+    /**
+     * Checks wether the given data is compatible with the HTMLElementModel class.
+     * @param panel The HTMLElementModel to verify
+     * @returns true when the given panel is compatible with HTMLElementModel, otherwise false
+     */
     static isValid(o: HTMLElementModel): Boolean {
         if (!o) return false;
         if (!o.tagname) return false;
@@ -155,6 +222,12 @@ class HTMLElementModel {
         // TODO investigate more transparent ways to do runtime type checking (https://github.com/fabiandev/ts-runtime)
         return true
     }
+
+    /**
+     * Creates an HTMLElement compatible with the given description
+     * @param model
+     * @returns the HTMLElement created
+     */
     static createElement(model: HTMLElementModel): HTMLElement {
         let e = document.createElement(model.tagname);
         if (!e) throw new Error("Cant create `" + model.tagname + "` in document");
@@ -178,10 +251,21 @@ class HTMLElementModel {
     }
 }
 
+/**
+ * describe an interface to access a DOM
+ */
 interface IDOM {
+    /**
+     * returns the HTMLElement with the given id in the accessed DOM
+     * @param id 
+     */
     findById(id: string): HTMLElement
 }
 
+/**
+ * Represents an interface to access the standard `document` DOM
+ * @implements IDOM
+ */
 class DOM implements IDOM {
     findById(id: string): HTMLElement {
         let e = document.getElementById(id);
